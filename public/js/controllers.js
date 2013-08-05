0718b27bc0ed909ba542545d1db47b271e5c3fc8
Jonas Mora <jonas.mora@outboxlabs.com>
2013-08-05 16:34:59 -0300

    Use angular google maps plugin

--- public/js/controllers.js
+++ public/js/controllers.js
@@ -1,2 +1,16 @@
+(function () {
+  var module = angular.module("letsMeet", ["google-maps"]);
+}());
+
 function GeolocationListCtrl($scope) {
+
+  angular.extend($scope, {
+    center: {
+      latitude: 0, // initial map center latitude
+      longitude: 0, // initial map center longitude
+    },
+    markers: [], // an array of markers,
+    zoom: 8, // the zoom level
+  });
+
 }
