5731a04229fb6351fca2494f8c3d376b3e087cf5
Jonas Mora <jonas.mora@outboxlabs.com>
2013-08-07 12:02:11 -0300

    Change property names

--- public/js/controllers.js
+++ public/js/controllers.js
@@ -10,33 +10,19 @@ function GeolocationListCtrl($scope, $timeout, $http, $log) {
 
   angular.extend($scope, {
 
-    position: {
-      coords: {
-        latitude: 45,
-        longitude: -73
-      }
-    },
-
     /** the initial center of the map */
-    centerProperty: {
+    center: {
       latitude: 45,
       longitude: -73
     },
 
     /** the initial zoom level of the map */
-    zoomProperty: 12,
+    zoom: 12,
 
     /** list of markers to put in the map */
-    markersProperty: [{
-      latitude: 45,
-      longitude: -74
-    }],
+    markers: [],
 
-    // These 2 properties will be set when clicking on the map
-    clickedLatitudeProperty: null,
-    clickedLongitudeProperty: null,
-
-    eventsProperty: {
+    events: {
       click: function (mapModel, eventName, originalEventArgs) {
         // 'this' is the directive's scope
         $log.log("user defined event on map directive with scope", this);
